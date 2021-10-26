471c7649cc0822b3f434c01d5cbf48664cff3b7b
JUAN ESTEBAN GOMEZ ZULUAGA <jgomezzu@contratista.epm.co>
2021-10-26 17:14:03 -0500

    -Se realiza la sección 4 del modulo controlling program flow.

--- ControllingProgramFlow/Program.cs
+++ ControllingProgramFlow/Program.cs
@@ -2,32 +2,134 @@
 namespace ControllingProgramFlow
 {
     using System;
+    using System.Collections.Generic;
+    using System.IO;
 
     class Program
     {
         static void Main(string[] args)
         {
+            GenerateWinnerEmails();
+
+            GenerateTasksReport();
+
+            GenerateCommentsReport();
+
+        }
+
+        public static void GenerateWinnerEmails()
+        {
+            var selectedEmails = new List<string>();
+            int counter = 0;
+
+            Console.WriteLine(Environment.NewLine + "Selected Winners Output");
+            while (selectedEmails.Count < 2 && counter < Q1Results.Responses.Count)
+            {
+                var currentItem = Q1Results.Responses[counter];
+
+                if (currentItem.FavoriteProduct == "Cappucino")
+                {
+                    selectedEmails.Add(currentItem.EmailAddress);
+                    Console.WriteLine(currentItem.EmailAddress);
+                }
+
+                counter++;
+            }
+
+            File.WriteAllLines("WinnersReport.csv", selectedEmails);
+        }
+
+        public static void GenerateCommentsReport()
+        {
+            var comments = new List<string>();
+
+            Console.WriteLine(Environment.NewLine + "Comments Outpu");
+            for (int i = 0; i < Q1Results.Responses.Count; i++)
+            {
+                var currentResponse = Q1Results.Responses[i];
+
+                if (currentResponse.WouldRecommend < 7.0)
+                {
+                    Console.WriteLine(currentResponse.Comments);
+                    comments.Add(currentResponse.Comments);
+                }
+
+            }
+
+            foreach (var response in Q1Results.Responses)
+            {
+                if (response.AreaToImprove == Q1Results.AreaToImprove)
+                {
+                    Console.WriteLine(response.Comments);
+                    comments.Add(response.Comments);
+                }
+            }
+
+            File.WriteAllLines("CommentsReport.csv", comments);
+        }
+
+        public static void GenerateTasksReport()
+        {
+            var tasks = new List<string>();
+
             //Tasa de respuesta
             double responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed;
-            //Encuestas sin respuesta
-            double unansweredCount = Q1Results.NumberSurveyed - Q1Results.NumberResponded;
             double overallScore = (Q1Results.ServiceScore + Q1Results.CoffeeScore + Q1Results.PriceScore + Q1Results.FoodScore) / 4;
 
-            Console.WriteLine($"Response Percentage: {responseRate}");
-            Console.WriteLine($"Unanswered Surveys: {unansweredCount}");
-            Console.WriteLine($"Overall Score: {overallScore}");
-
             //Logical comparison
-            bool higherCoffeeScore = Q1Results.CoffeeScore > Q1Results.FoodScore;
-            bool customersRecommend = Q1Results.WouldRecommend >= 7;
-            bool noGranolaYesCappucino = Q1Results.LeastFavoriteProduct == "Granola" && Q1Results.FavoriteProduct == "Cappucino";
-            bool noGranolaYesCappucino2 = Q1Results.LeastFavoriteProduct.Equals("Granola") && Q1Results.FavoriteProduct.Equals("Cappucino");
+            if (Q1Results.CoffeeScore < Q1Results.FoodScore)
+            {
+                tasks.Add("Investigate coffe recipes and ingredients");
+            }
 
-            Console.WriteLine($"Coffe score Higher Than Food:  { higherCoffeeScore}");
-            Console.WriteLine($"Customers Would Recommend Us:  { customersRecommend}");
-            Console.WriteLine($"Hate Granola, Love Cappucino:  { noGranolaYesCappucino}");
-            Console.WriteLine($"Hate Granola, Love Cappucino:  { noGranolaYesCappucino2}");
+            if (overallScore > 8.0)
+            {
+                tasks.Add("Work with leadership to reward staff");
+            }
+            else
+            {
+                tasks.Add("Work with employees for improvement ideas");
+            }
 
+            if (responseRate < .33)
+            {
+                tasks.Add("Research options to improve response rate.");
+            }
+            else if (responseRate > .33 && responseRate < .66)
+            {
+                tasks.Add("Reward participants with free coffe coupon");
+            }
+            else
+            {
+                tasks.Add("Rewards participants with discount coffe coupon.");
+            }
+
+            switch (Q1Results.AreaToImprove)
+            {
+                case "RewardsProgram":
+                    tasks.Add("Revisit the rewards deals");
+                    break;
+
+                case "Cleanliness":
+                    tasks.Add("Contact the cleaning vendor.");
+                    break;
+
+                case "MobileApp":
+                    tasks.Add("Contact consulting firm about app.");
+                    break;
+
+                default:
+                    tasks.Add("Investigate individual comments for ideas.");
+                    break;
+            }
+
+            Console.WriteLine(Environment.NewLine + "Tasks Output");
+            foreach (var task in tasks)
+            {
+                Console.WriteLine(task);
+            }
+
+            File.WriteAllLines("TasksReport.csv", tasks);
         }
     }
 }
---
+++ ControllingProgramFlow/SurveyResponse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControllingProgramFlow
+{
+    public class SurveyResponse
+    {
+        public double ServiceScore { get; set; }
+
+        public double CoffeeScore { get; set; }
+
+        public double PriceScore { get; set; }
+
+        public double FoodScore { get; set; }
+
+        public double WouldRecommend { get; set; }
+
+        public string FavoriteProduct { get; set; }
+
+        public string LeastFavoriteProduct { get; set; }
+
+        public string AreaToImprove { get; set; }
+
+        public bool IsRewardsMember { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string Comments { get; set; }
+    }
+}
