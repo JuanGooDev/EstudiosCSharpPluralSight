da4ccfa18c4adfb8b73eab21ef6614fffb777589
Juan Esteban Gómez Zuluaga <jgomezzu@contratista.epm.co>
2021-11-10 09:14:00 -0500

    -Se finaliza el modulo 6 donde se habla de tipos de valor y tipos de referencia, se trabaja en las clases tipo enum y tipo struct.

---
+++ StructClass/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace StructClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UsingAStruct();
+        }
+
+        private static void UsingAStruct()
+        {
+            Employee employee;
+            employee.Name = "Bethany";
+            employee.Wage = 1250;
+            employee.Work();
+        }
+
+        struct Employee
+        {
+            public string Name;
+            public int Wage;
+
+            public void Work()
+            {
+                Console.WriteLine($"{Name} is now doing work!");
+            }
+        }
+
+    }
+}
---
+++ TipoSistema/Program.cs
@@ -0,0 +1,67 @@
+﻿
+namespace EnumerationClass
+{
+    using System;
+
+    /// <summary>
+    /// En esta clase se desarrollarán ejemplos relacionados a Tipos de sistemas y tipos personalizados
+    /// de sistemas.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UsingEnumerations();
+
+            Console.ReadLine();
+        }
+
+        private static void UsingEnumerations()
+        {
+            EmployeeType employeeType = EmployeeType.Manager;
+            StoreType storeType = StoreType.Seating;
+            int baseWage = 1000;
+
+            CalculateWage(baseWage, employeeType, storeType);
+        }
+
+        private static void CalculateWage(int baseWage, EmployeeType employeeType, StoreType storeType)
+        {
+            int calculatedWage = 0;
+            Console.WriteLine($"Employee type: {employeeType}");
+            Console.WriteLine($"Store type: {storeType}");
+
+            if (employeeType == EmployeeType.Manager)
+            {
+                calculatedWage = baseWage * 3;
+            }
+            else
+            {
+                calculatedWage *= 2;
+            }
+
+            if (storeType == StoreType.FullPieRestaurant)//Larger retail point so give a bonus
+                calculatedWage += 500;
+
+
+            Console.WriteLine($"The calculated wage is {calculatedWage}");
+        }
+
+    }
+
+    enum EmployeeType
+    {
+        Sales,//0
+        Manager,//1
+        Research,//2
+        StoreManager//3
+    }
+
+    enum StoreType
+    {
+        PieCorner = 10,
+        Seating = 20,
+        FullPieRestaurant = 100,
+        Undefined = 99
+    }
+}
